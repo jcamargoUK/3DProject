1560b6811324a797326238ae3162fa146c2f75dc
Julio Camargo <jcamargo@hotmail.co.uk>
2023-03-15 10:41:52 +0000

    computers section complete

--- 3D-Project/src/components/canvas/Computers.jsx
+++ 3D-Project/src/components/canvas/Computers.jsx
@@ -1,21 +1,21 @@
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from "../Loader";
+import Loader from "../Loader";
 
 const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
+      <hemisphereLight intensity={0.35} groundColor="black" />
+      <pointLight intensity={1.5} />
       <spotLight
-      position={[-20, 50, 10]}
+      position={[-55, 50, 60]}
       />      
       <primitive object={computer.scene}
-      scale={0.75}
-      position={[0, -9.25, -1.5]}
-      rotation={[-0.01, -0.2, -0.1]} />
+      scale={0.65}
+      position={[-2, -6, -1]}
+      rotation={[0, 0, 0]} />
     </mesh>
   );
 };
@@ -25,12 +25,13 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 3, 5], fov: 70 }}
       gl={{ preserveDrawingBuffer: true}}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense fallback={<Loader />}>
         <OrbitControls  
-        enabledZoom={false}
+        enableZoom={false}
+        minZoom={2}
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         />
